623aefe6d547769b7aa9e3b450b209db4896ea60
Amir Hafizovic <hafizovic.amir@gmail.com>
2018-02-07 13:27:06 +1100

    deleted files

--- ba-app/src/index.js
+++ ba-app/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from './Routes';
+
+ReactDOM.render(Routes, document.getElementById('root'));
 registerServiceWorker();
