f70afc8ca415fae13998a3950dd7ffe8457bc006
Amir Hafizovic <hafizovic.amir@gmail.com>
2018-02-07 22:03:21 +1100

    passing api data through components

--- ba-app/src/Routes.js
+++ ba-app/src/Routes.js
@@ -3,12 +3,13 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Flights from './components/Flights';
+import Reservations from './components/Reservations';
 
 const Routes = (
     <Router>
         <div>
         <Route exact path="/" component={ Home }/>
-        <Route path="/flights/:id" component={ Flights }/>
+        <Route path="/flights/:id" component={ Reservations }/>
         <Route exact path="/flights" component={ Flights }/>
         </div>
     </Router>
--- ba-app/src/components/FlightList.js
+++ ba-app/src/components/FlightList.js
@@ -2,6 +2,11 @@ import React, {Component } from 'react';
 
 import { Link } from 'react-router-dom';
 
+const findPlane = function (planes, flight) {
+  console.log('in findPlane', planes.filter(plane => plane.id == flight.airplane_id)[0]);
+  return planes.filter(plane => plane.id == flight.airplane_id)[0];
+}
+
 class FlightList extends Component {
   constructor(props) {
     super(props);
@@ -11,22 +16,26 @@ class FlightList extends Component {
 
 
   render() {
+    const { planes_all, flights_all } = this.props;
     return (
       <div>
         <h3>Search Results</h3>
 
-        {this.props.flights_all.map (s=>
-
-
-          <div>
-
-
-
-          <p>{s.origin} to {s.destination} on {s.date} <Link to= {s.flight_no}>{s.flight_no}</Link></p>
-
-          <input type = "submit" value="Book Now!" />
-
-          </div>
+        {flights_all.map (s => {
+          const url = `/flights/${s.id.toString()}`
+          const plane = findPlane(planes_all, s);
+          return (
+              <div>
+                <p>
+                  {s.origin} to {s.destination} on {s.date} on plane {plane.name}
+                  <Link to={{
+                   pathname: url,
+                   state: { flight: s, plane: plane}
+                 }}>Flight {s.id}</Link>
+                </p>
+              </div>
+          )
+        }
         )}
       </div>
     );
